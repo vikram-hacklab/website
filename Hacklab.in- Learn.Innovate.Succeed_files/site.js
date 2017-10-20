434e1cd11e482ee7d24d53e05556a3108e71ea4b
sarwadenj <sarwadenj555@gmail.com>
2017-10-20 22:02:56 +0530

    new respository

---
+++ Hacklab.in- Learn.Innovate.Succeed_files/site.js
@@ -0,0 +1,22 @@
+
+	jQuery(document).ready(function($) {
+
+		    //$('#nav-main').scrollspy()
+		    
+		    // Localscrolling 
+    		$('#nav-main, .brand').localScroll();
+     		$('#news, .container').localScroll();
+
+	});
+	
+	
+
+
+/*
+     FILE ARCHIVED ON 19:01:58 Sep 12, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:02:49 Oct 20, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
